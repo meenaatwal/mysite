0b71990d8039a652c49cc4efea7fb6442bc18757
meenaatwal <150340651+meenaatwal@users.noreply.github.com>
2024-02-12 13:56:53 +0530

    Add files via upload
    angular app

---
+++ cloudflare/_worker.js/index.js
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3PTVUMY5.mjs";import{H as s}from"./chunk-C3OKC3ZD.mjs";import{f as r}from"./chunk-HQUS2UUF.mjs";function p(e,n){return r(this,null,function*(){let o=new URL(e.url);console.log("render SSR",o.href);let c=new URL("/",o),t=yield n.ASSETS.fetch(new Request(c)),l=yield t.text(),i=yield s(a,{document:l,url:o.pathname});return new Response(i,t)})}var w={fetch:(e,n)=>globalThis.__zone_symbol__Promise.resolve(p(e,n))};export{w as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3PTVUMY5.mjs";import"./chunk-C3OKC3ZD.mjs";import"./chunk-HQUS2UUF.mjs";export{a as default};
